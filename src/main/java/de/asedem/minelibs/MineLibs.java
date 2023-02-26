fc5dda2dae26f540bb5a756b118c28a4d067a383
Asedem <simon.oberzier@juniorjob.de>
2023-02-26 02:05:46 +0100

    Removed the AsyncPlayerMoveEvent

--- src/main/java/de/asedem/minelibs/MineLibs.java
+++ src/main/java/de/asedem/minelibs/MineLibs.java
@@ -1,7 +1,6 @@
 package de.asedem.minelibs;
 
 import de.asedem.minelibs.inventory.InventoryClickListener;
-import de.asedem.minelibs.movement.AsyncPlayerMoveListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
@@ -16,15 +15,5 @@ public class MineLibs {
      */
     public static void initInventoryClickModule(@NotNull Plugin plugin) {
         Bukkit.getPluginManager().registerEvents(new InventoryClickListener(), plugin);
-    }
-
-    /**
-     * Inits the AsyncPlayerMoveModule.
-     * Without this the AsyncPlayerMoveEvent will not work.
-     *
-     * @param plugin The Plugin, where the API is used in.
-     */
-    public static void initAsyncPlayerMoveEventModule(@NotNull Plugin plugin) {
-        AsyncPlayerMoveListener.activate(plugin);
     }
 }
