f72e988397edd40c9d121243a3b6e4a01bdf9d27
Asedem <simon.oberzier@juniorjob.de>
2023-03-05 01:26:27 +0100

    Added a Methode to get the waypoints

--- src/main/java/de/asedem/minelibs/math/Region.java
+++ src/main/java/de/asedem/minelibs/math/Region.java
@@ -1,12 +1,14 @@
 package de.asedem.minelibs.math;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -37,6 +39,19 @@ public record Region(
     @NotNull
     public static Point locationToPoint(@NotNull Location location) {
         return new Point(location.getBlockX(), location.getBlockZ());
+    }
+
+    /**
+     * Converts a Point from java.awt to a Bukkit location
+     *
+     * @param point the Point from java.awt
+     * @param world the world to convert
+     * @param height the height of the location
+     * @return the Bukkit location
+     */
+    @NotNull
+    public static Location pointToLocation(@NotNull Point point, @NotNull World world, int height) {
+        return new Location(world, point.getX(), height, point.getY());
     }
 
     /**
@@ -85,9 +100,53 @@ public record Region(
             Point next;
             if (i == this.points().size() - 1) next = this.points().get(0);
             else next = this.points().get(i + 1);
-            vectors.add(new Vector(next.x - self.x, heightVariation, next.y - self.y));
+            vectors.add(this.toVector(self, next, heightVariation));
         }
         return vectors;
+    }
+
+    /**
+     * Draws points around the given edgePoints
+     *
+     * @param space the space between the generated points
+     * @return the generated points
+     */
+    @NotNull
+    public List<Point> wayPoints(int space) {
+        if (this.invalid()) return Collections.emptyList();
+        final List<Point> wayPoints = new ArrayList<>();
+        for (int i = 0; i < this.points().size(); i++) {
+            final Point self = this.points().get(i);
+            Point next;
+            if (i == this.points().size() - 1) next = this.points().get(0);
+            else next = this.points().get(i + 1);
+            final double distance = this.toVector(self, next, 0).length();
+            final Vector selfVector = new Vector(self.x, 0, self.y);
+            final Vector nextVector = new Vector(next.x, 0, next.y);
+            final Vector vec = nextVector.clone()
+                    .subtract(selfVector)
+                    .normalize()
+                    .multiply(space);
+            wayPoints.add(new Point(selfVector.getBlockX(), selfVector.getBlockZ()));
+            for (double j = 0; j < distance; selfVector.add(vec)) {
+                wayPoints.add(new Point(selfVector.getBlockX(), selfVector.getBlockZ()));
+                j += space;
+            }
+        }
+        return wayPoints;
+    }
+
+    /**
+     * ConvertsTwo points to a vector
+     *
+     * @param self the fist point to convert
+     * @param next the second point to convert
+     * @param heightVariation the variation in height
+     * @return the responded vector
+     */
+    @NotNull
+    private Vector toVector(@NotNull Point self, @NotNull Point next, int heightVariation) {
+        return new Vector(next.x - self.x, heightVariation, next.y - self.y);
     }
 
     /**
